7848cc6daef56694119c1355df00c4e989bfb398
BlueHillBGCB <mmcmahon@bgcb.org>
2016-05-17 17:17:00 -0400

    upload_RPI_videoslideshow

---
+++ BlueHillTV.sh
@@ -0,0 +1,21 @@
+# BlueHillTV.sh
+# Michael McMahon
+# This acts as a digital slideshow for the entrance way.  It rotates between a picture slideshow and videos.
+
+feh -FZY ~/BlueHillTV/WelcomeSpring2016/001WelcomeToTheBlueHillClub1080.png &
+
+while true
+do
+	feh -FZYD 5 --cycle-once ~/BlueHillTV/WelcomeSpring2016/*.png
+	omxplayer -I -o hdmi ~/BlueHillTV/Video/*.mov
+done
+
+# feh switches
+# -F	Fullscreen
+# -Z	auto-Zoom
+# -Y	hide pointer
+# -D	Delay NumberOfSeconds
+
+# omxplayer switches
+# -I		Info before playback
+# -o hdmi	audio Output device as hdmi
