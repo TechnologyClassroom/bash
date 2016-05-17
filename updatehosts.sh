93eabb6d851918649ca64db7a9eeae89b012b439
BlueHillBGCB <mmcmahon@bgcb.org>
2016-05-17 15:39:25 -0400

    update_updatehosts.sh

--- updatehosts.sh
+++ updatehosts.sh
@@ -1,2 +1,10 @@
+# updatehosts.sh
+# This script downloads HOSTS files from github.com/BlueHillBGCB/HOSTS on GNU/Linux, Mac, router & Unix systems.
+# Michael McMahon
+
+# Download HOSTS files from github.com/BlueHillBGCB/HOSTS
 curl -o /etc/HOSTSFunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunix.txt
 curl -o /etc/HOSTSMTWRunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunix.txt
+
+# Copy MTWR file to the Hosts file.
+cp /etc/HOSTSMTWRunix.txt /etc/hosts
