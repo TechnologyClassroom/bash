9588abea10a19f07652d230f51c6dbd14e2bd60e
BlueHillBGCB <mmcmahon@bgcb.org>
2016-05-23 17:19:09 -0400

    addLS

--- updatehosts.sh
+++ updatehosts.sh
@@ -5,6 +5,8 @@
 # Download HOSTS files from github.com/BlueHillBGCB/HOSTS
 curl -o /etc/HOSTSFunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunix.txt
 curl -o /etc/HOSTSMTWRunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunix.txt
+curl -o /etc/HOSTSFunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunixLS.txt
+curl -o /etc/HOSTSMTWRunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunixLS.txt
 
 # Copy MTWR file to the Hosts file.
 cp /etc/HOSTSMTWRunix.txt /etc/hosts
