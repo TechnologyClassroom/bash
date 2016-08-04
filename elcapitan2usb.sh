d761e24ebfb37340272c4eea37f785f647b304e4
Michael McMahon <bgcbcomputerclubhouse@gmail.com>
2016-08-04 10:18:22 -0400

    OS X to USB scripts

---
+++ elcapitan2usb.sh
@@ -0,0 +1,10 @@
+# ElCapitan2USB.sh
+# Michael McMahon
+# Copy Mac OS X El Capitan installer to an 8GB USB flash drive
+
+# Once complete, you can plug it into another Mac.  After you press the power button, wait for the sound and press the ALT or OPTION key to bring up the boot menu.
+
+sudo /Applications/Install\ OS\ X\ El\ Capitan.app/Contents/Resources/createinstallmedia --volume /Volumes/Untitled --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app --nointeraction
+
+# based on http://forums.macrumors.com/threads/how-to-make-a-bootable-install-of-mavericks-on-flash-usb-drive.1649986/#post-18081307
+
---
+++ mavericks2usb.sh
@@ -0,0 +1,10 @@
+# Mavericks2USB.sh
+# Michael McMahon
+# Copy Mac OS X Mavericks installer to an 8GB USB flash drive
+
+# Once complete, you can plug it into another Mac.  After you press the power button, wait for the sound and press the ALT or OPTION key to bring up the boot menu.
+
+sudo /Applications/Install\ OS\ X\ Mavericks.app/Contents/Resources/createinstallmedia --volume /Volumes/Untitled --applicationpath /Applications/Install\ OS\ X\ Mavericks.app --nointeraction
+
+# based on http://forums.macrumors.com/threads/how-to-make-a-bootable-install-of-mavericks-on-flash-usb-drive.1649986/#post-18081307
+
