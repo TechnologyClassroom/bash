73f4ea1ee00cf091b5cd0e47eb873f1731c94349
Michael McMahon <michael@gnu.org>
2022-11-11 14:08:04 -0500

    Upload svg2web

---
+++ svg2web.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# svg2web
+# Michael McMahon 2022
+# Convert all SVG files recursively to PNG and WebP with multiple resolutions for modern web development.
+
+# Depends on BASH, sed, Inkscape 1.2, and cwebp.
+#   sudo apt install -y inkscape webp
+
+convertsvgforweb() {
+  # Generate default 96 dpi (1x).
+  inkscape -C $1 -o "${1%.svg}.png"
+  cwebp -progress -lossless -m 6 -z 9 -q 100 "${1%.svg}.png" -o "${1%.svg}.webp"
+
+  # Generate default 192 dpi (2x).
+  inkscape -C $1 -d 192 -o "${1%.svg}-2x.png"
+  cwebp -progress -lossless -m 6 -z 9 -q 100 "${1%.svg}-2x.png" -o "${1%.svg}-2x.webp"
+
+  # Generate default 288 dpi (3x).
+  inkscape -C $1 -d 288 -o "${1%.svg}-3x.png"
+  cwebp -progress -lossless -m 6 -z 9 -q 100 "${1%.svg}-3x.png" -o "${1%.svg}-3x.webp"
+
+  # Generate example picture tag.
+  IMAGE_DIR="img/"
+  echo -e "<picture>\n  <source\n    type=\"image/webp\"\n    srcset=\"${IMAGE_DIR}${1%.svg}.webp 1x, ${IMAGE_DIR}${1%.svg}-2x.webp 2x, ${IMAGE_DIR}${1%.svg}-3x.webp 3x\">\n  <source\n    type=\"image/png\"\n    srcset=\"${IMAGE_DIR}${1%.svg}.png 1x, ${IMAGE_DIR}${1%.svg}-2x.png 2x, ${IMAGE_DIR}${1%.svg}-3x.png 3x\">\n  <img src=\"${IMAGE_DIR}$1\"\n    alt=\"\">\n</picture>" > ${1%.svg}.html
+  sed -i 's|/./|/|g' ${1%.svg}.html
+}
+
+export -f convertsvgforweb
+find ./ -type f -name '*.svg' -exec bash -c 'convertsvgforweb "$0"' {} \;
